de030fbea3d460a60a8964559ae279de6c72665c
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-26 20:59:31 +0530

    Remove UserRole and UserRolePrimaryKey

--- user/src/main/java/com/cryptopos/user/entity/UserRole.java
+++
@@ -1,9 +0,0 @@
-package com.cryptopos.user.entity;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
-@Table("user_role")
-public record UserRole(@Id UserRolePrimaryKey id) {
-
-}
--- user/src/main/java/com/cryptopos/user/entity/UserRolePrimaryKey.java
+++
@@ -1,7 +0,0 @@
-package com.cryptopos.user.entity;
-
-import java.util.UUID;
-
-public record UserRolePrimaryKey(UUID userId, UUID roleId) {
-
-}

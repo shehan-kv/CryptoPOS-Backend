fa75eb6857a2a167bdb12508d7789a775f4bc57b
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-26 20:20:48 +0530

    Create User entity

---
+++ user/src/main/java/com/cryptopos/user/entity/User.java
@@ -0,0 +1,17 @@
+package com.cryptopos.user.entity;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("users")
+public record User(
+        @Id UUID id,
+        String firstName,
+        String lastName,
+        String email,
+        String password,
+        boolean isActive,
+        boolean isVerified) {
+}

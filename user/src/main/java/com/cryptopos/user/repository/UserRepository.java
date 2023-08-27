5f1ab29043465d1f378345727e2bff42515c30ff
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 16:31:51 +0530

    Update type of primary key in UserRepository

--- user/src/main/java/com/cryptopos/user/repository/UserRepository.java
+++ user/src/main/java/com/cryptopos/user/repository/UserRepository.java
@@ -1,14 +1,12 @@
 package com.cryptopos.user.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.cryptopos.user.entity.User;
 
 import reactor.core.publisher.Mono;
 
-public interface UserRepository extends ReactiveCrudRepository<User, UUID> {
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
 
     Mono<User> findByEmail(String email);
 }

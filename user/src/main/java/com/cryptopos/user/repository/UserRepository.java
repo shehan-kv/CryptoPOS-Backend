dd0c5e45085a63372c1e93816b5d92036dd853a1
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 11:02:15 +0530

    Define findByEmail method in UserRepository

--- user/src/main/java/com/cryptopos/user/repository/UserRepository.java
+++ user/src/main/java/com/cryptopos/user/repository/UserRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.cryptopos.user.entity.User;
 
+import reactor.core.publisher.Mono;
+
 public interface UserRepository extends ReactiveCrudRepository<User, UUID> {
 
+    Mono<User> findByEmail(String email);
 }

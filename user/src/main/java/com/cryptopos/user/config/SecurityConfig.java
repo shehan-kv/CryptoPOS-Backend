8114c56844a1fe17f9d3589c58d83ef934029e57
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 16:30:39 +0530

    Add signup route with permitAll permission

--- user/src/main/java/com/cryptopos/user/config/SecurityConfig.java
+++ user/src/main/java/com/cryptopos/user/config/SecurityConfig.java
@@ -1,8 +1,23 @@
 package com.cryptopos.user.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(authorize -> authorize
+                        .pathMatchers(
+                                "/signup")
+                        .permitAll()
+
+                )
+                .build();
+    }
 }

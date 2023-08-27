37e35d41b9a92e0d01957b503f57efbbc9427510
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 16:31:37 +0530

    Update type of primary key in RoleRepository

--- user/src/main/java/com/cryptopos/user/repository/RoleRepository.java
+++ user/src/main/java/com/cryptopos/user/repository/RoleRepository.java
@@ -1,14 +1,12 @@
 package com.cryptopos.user.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.cryptopos.user.entity.Role;
 
 import reactor.core.publisher.Mono;
 
-public interface RoleRepository extends ReactiveCrudRepository<Role, UUID> {
+public interface RoleRepository extends ReactiveCrudRepository<Role, Long> {
 
     Mono<Role> findByName(String name);
 }

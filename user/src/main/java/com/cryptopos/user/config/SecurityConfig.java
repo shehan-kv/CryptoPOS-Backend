c515f265807c8dd7ab256e6a11b88f1ead9678d1
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 16:46:06 +0530

    Add PasswordEncoder bean

--- user/src/main/java/com/cryptopos/user/config/SecurityConfig.java
+++ user/src/main/java/com/cryptopos/user/config/SecurityConfig.java
@@ -3,10 +3,17 @@ package com.cryptopos.user.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Bean
     public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http) {

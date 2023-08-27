0364104c9a90854febc51908e64132d405f075fa
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 16:31:02 +0530

    Update id type of Role

--- user/src/main/java/com/cryptopos/user/entity/Role.java
+++ user/src/main/java/com/cryptopos/user/entity/Role.java
@@ -1,13 +1,11 @@
 package com.cryptopos.user.entity;
 
-import java.util.UUID;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("roles")
 public record Role(
-        @Id UUID id,
+        @Id Long id,
         String name) {
 
 }

224ec45832ab6bd847e9464d7a88a092cadb6030
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 10:32:13 +0530

    Add signUp handler method

--- user/src/main/java/com/cryptopos/user/handler/UserHandler.java
+++ user/src/main/java/com/cryptopos/user/handler/UserHandler.java
@@ -1,8 +1,14 @@
 package com.cryptopos.user.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.cryptopos.user.dto.SignUpRequest;
 import com.cryptopos.user.service.user.UserService;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class UserHandler {
@@ -11,5 +17,13 @@ public class UserHandler {
 
     public UserHandler(UserService uService) {
         this.userService = uService;
+    }
+
+    public Mono<ServerResponse> signUp(ServerRequest request) {
+        return request
+                .bodyToMono(SignUpRequest.class)
+                .flatMap(signUpRequest -> userService.signUp(signUpRequest))
+                .flatMap(result -> result ? ServerResponse.ok().build()
+                        : ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 }

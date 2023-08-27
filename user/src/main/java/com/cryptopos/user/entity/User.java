e182f040a4e5c692cf23afec4a0eac8720720681
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-27 16:31:14 +0530

    Update id type of User

--- user/src/main/java/com/cryptopos/user/entity/User.java
+++ user/src/main/java/com/cryptopos/user/entity/User.java
@@ -1,18 +1,16 @@
 package com.cryptopos.user.entity;
 
-import java.util.UUID;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("users")
 public record User(
-        @Id UUID id,
+        @Id Long id,
         String firstName,
         String lastName,
         String email,
         String password,
         boolean isActive,
         boolean isVerified,
-        UUID roleId) {
+        Long roleId) {
 }

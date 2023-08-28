e0a8ed590df30f1d1a5475c5423dcb111e5d5d46
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-28 21:13:24 +0530

    Configure formlogin and logout in users

--- user/src/main/java/com/cryptopos/user/config/SecurityConfig.java
+++ user/src/main/java/com/cryptopos/user/config/SecurityConfig.java
@@ -2,10 +2,22 @@ package com.cryptopos.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
+import org.springframework.security.web.server.authentication.ServerAuthenticationEntryPointFailureHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.HttpStatusReturningServerLogoutSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class SecurityConfig {
@@ -25,6 +37,33 @@ public class SecurityConfig {
                         .permitAll()
 
                 )
+                .formLogin(customizer -> customizer
+                        .authenticationSuccessHandler(new ServerAuthenticationSuccessHandler() {
+                            @Override
+                            public Mono<Void> onAuthenticationSuccess(
+                                    WebFilterExchange webFilterExchange,
+                                    Authentication authentication) {
+                                return Mono.fromRunnable(() -> webFilterExchange
+                                        .getExchange().getResponse()
+                                        .setStatusCode(HttpStatus.OK));
+                            }
+                        })
+                        .authenticationFailureHandler(
+                                new ServerAuthenticationEntryPointFailureHandler(
+                                        new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
+
+                        .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+
+                        .requiresAuthenticationMatcher(
+                                new PathPatternParserServerWebExchangeMatcher("/signin", HttpMethod.POST))
+
+                )
+                .logout(customizer -> customizer
+                        .logoutSuccessHandler(
+                                new HttpStatusReturningServerLogoutSuccessHandler())
+                        .logoutHandler(new WebSessionServerLogoutHandler())
+                        .requiresLogout(new PathPatternParserServerWebExchangeMatcher(
+                                "/signout", HttpMethod.GET)))
                 .build();
     }
 }

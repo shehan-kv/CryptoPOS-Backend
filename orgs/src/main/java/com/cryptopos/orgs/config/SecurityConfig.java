f4ec09a0a20e3980acba88647693df8be0ce0a3b
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-30 20:50:42 +0530

    Create SecurityConfig in orgs

---
+++ orgs/src/main/java/com/cryptopos/orgs/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.cryptopos.orgs.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public MapReactiveUserDetailsService mapReactiveUserDetailsService() {
+        return new MapReactiveUserDetailsService(Collections.emptyMap());
+    }
+
+    @Bean
+    public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(authorize -> authorize
+                        .pathMatchers("/hello")
+                        .hasRole("GLOBAL_ADMINISTRATOR")
+
+                )
+                .formLogin(customizer -> customizer.disable())
+                .httpBasic(customizer -> customizer.disable())
+                .build();
+    }
+}

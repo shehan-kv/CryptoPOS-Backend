585ca6c8051f7d34b89c6d689478fef36948a01d
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-30 11:57:54 +0530

    Create a microservice to manage organizations

---
+++ orgs/src/main/java/com/cryptopos/orgs/OrgsApplication.java
@@ -0,0 +1,13 @@
+package com.cryptopos.orgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgsApplication.class, args);
+	}
+
+}

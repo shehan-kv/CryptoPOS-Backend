6cf4b133b1bdd5a2809a96e973634fcb0cac6ec5
Shehan Maduwantha <kvsm066@gmail.com>
2023-08-31 17:46:11 +0530

    Define findByCode method in country, currency repo

--- orgs/src/main/java/com/cryptopos/orgs/repository/CountryRepository.java
+++ orgs/src/main/java/com/cryptopos/orgs/repository/CountryRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.cryptopos.orgs.entity.Country;
 
+import reactor.core.publisher.Mono;
+
 public interface CountryRepository extends ReactiveCrudRepository<Country, Long> {
 
+    Mono<Country> findByCode(String code);
 }
--- orgs/src/main/java/com/cryptopos/orgs/repository/CurrencyRepository.java
+++ orgs/src/main/java/com/cryptopos/orgs/repository/CurrencyRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.cryptopos.orgs.entity.Currency;
 
+import reactor.core.publisher.Mono;
+
 public interface CurrencyRepository extends ReactiveCrudRepository<Currency, Long> {
 
+    Mono<Currency> findByCode(String code);
 }

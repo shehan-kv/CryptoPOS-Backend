add6675dac25e22ec99b2378703be96b8107c6be
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-01 07:53:52 +0530

    Add saveOrg in EmployeeRepository

--- orgs/src/main/java/com/cryptopos/orgs/repository/EmployeeRepository.java
+++ orgs/src/main/java/com/cryptopos/orgs/repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.cryptopos.orgs.repository;
 import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.stereotype.Component;
 
+import reactor.core.publisher.Mono;
+
 @Component
 public class EmployeeRepository {
 
@@ -10,6 +12,16 @@ public class EmployeeRepository {
 
     public EmployeeRepository(DatabaseClient dClient) {
         this.databaseClient = dClient;
+    }
+
+    public Mono<Long> saveOrg(Long userId, Long orgId) {
+
+        return databaseClient
+                .sql("INSERT INTO employee_orgs (employee_id, org_id) VALUES (:userId, :orgId)")
+                .bind("userId", userId)
+                .bind("orgId", orgId)
+                .fetch()
+                .rowsUpdated();
     }
 
 }

83074e9cf7e9026eb6bf0bdec9fa3f5ba384a7fc
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-01 08:11:01 +0530

    Add createOrg handler method

--- orgs/src/main/java/com/cryptopos/orgs/handler/OrgHandler.java
+++ orgs/src/main/java/com/cryptopos/orgs/handler/OrgHandler.java
@@ -1,8 +1,29 @@
 package com.cryptopos.orgs.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.cryptopos.orgs.dto.CreateOrgRequest;
+import com.cryptopos.orgs.service.org.OrgService;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class OrgHandler {
 
+    private final OrgService orgService;
+
+    public OrgHandler(OrgService orgService) {
+        this.orgService = orgService;
+    }
+
+    public Mono<ServerResponse> createOrg(ServerRequest request) {
+        return request
+                .bodyToMono(CreateOrgRequest.class)
+                .flatMap(createRequest -> orgService.createOrg(createRequest))
+                .flatMap(createResult -> ServerResponse.status(HttpStatus.CREATED).build())
+                .onErrorResume(error -> ServerResponse.badRequest().build());
+    }
 }

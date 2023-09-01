305a344ee6d71a4c63236b10148a7f66f49b6699
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-01 07:57:03 +0530

    Define createOrg in OrgService

--- orgs/src/main/java/com/cryptopos/orgs/service/org/OrgService.java
+++ orgs/src/main/java/com/cryptopos/orgs/service/org/OrgService.java
@@ -2,7 +2,12 @@ package com.cryptopos.orgs.service.org;
 
 import org.springframework.stereotype.Service;
 
+import com.cryptopos.orgs.dto.CreateOrgRequest;
+
+import reactor.core.publisher.Mono;
+
 @Service
 public interface OrgService {
 
+    Mono<Boolean> createOrg(CreateOrgRequest createRequest);
 }

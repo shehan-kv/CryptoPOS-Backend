53c1245b5e029867e1bed82952692cc84859dbfa
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-05 21:46:29 +0530

    Define getOrgDetailsByUser method in OrgService

--- orgs/src/main/java/com/cryptopos/orgs/service/org/OrgService.java
+++ orgs/src/main/java/com/cryptopos/orgs/service/org/OrgService.java
@@ -1,8 +1,12 @@
 package com.cryptopos.orgs.service.org;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.cryptopos.orgs.dto.CreateOrgRequest;
+import com.cryptopos.orgs.dto.OrgDetailsResponse;
+import com.cryptopos.orgs.dto.Page;
 
 import reactor.core.publisher.Mono;
 
@@ -10,4 +14,6 @@ import reactor.core.publisher.Mono;
 public interface OrgService {
 
     Mono<Boolean> createOrg(CreateOrgRequest createRequest);
+
+    Mono<Page<OrgDetailsResponse>> getOrgDetailsByUser(Optional<String> pageNum, Optional<String> pageSize);
 }

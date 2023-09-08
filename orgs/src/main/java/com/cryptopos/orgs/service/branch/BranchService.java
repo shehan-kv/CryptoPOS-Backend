9d59c8398ae95b856d289dd4d4f6c96db52fab0e
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-08 19:46:06 +0530

    Define createBranch method in BranchService

--- orgs/src/main/java/com/cryptopos/orgs/service/branch/BranchService.java
+++ orgs/src/main/java/com/cryptopos/orgs/service/branch/BranchService.java
@@ -2,7 +2,13 @@ package com.cryptopos.orgs.service.branch;
 
 import org.springframework.stereotype.Service;
 
+import com.cryptopos.orgs.dto.BranchCreateRequest;
+import com.cryptopos.orgs.dto.BranchCreateResult;
+
+import reactor.core.publisher.Mono;
+
 @Service
 public interface BranchService {
 
+    Mono<BranchCreateResult> createBranch(BranchCreateRequest createRequest);
 }

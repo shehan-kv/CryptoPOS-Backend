a835c940dceff9f79def66a1f3c3897aad31f55e
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-09 11:32:39 +0530

    Create inventory SessionConfig

---
+++ inventory/src/main/java/com/cryptopos/inventory/config/SessionConfig.java
@@ -0,0 +1,17 @@
+package com.cryptopos.inventory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.session.CookieWebSessionIdResolver;
+import org.springframework.web.server.session.WebSessionIdResolver;
+
+@Configuration
+public class SessionConfig {
+
+    @Bean
+    public WebSessionIdResolver webSessionIdResolver() {
+        CookieWebSessionIdResolver resolver = new CookieWebSessionIdResolver();
+        resolver.setCookieName("CRYPTOPOS_SESSION");
+        return resolver;
+    }
+}

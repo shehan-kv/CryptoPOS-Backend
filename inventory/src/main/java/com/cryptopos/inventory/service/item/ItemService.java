ed029e1231531d3c1be606901aa61e8304ec5297
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-09 13:17:37 +0530

    Create ItemService and implementation

---
+++ inventory/src/main/java/com/cryptopos/inventory/service/item/ItemService.java
@@ -0,0 +1,8 @@
+package com.cryptopos.inventory.service.item;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ItemService {
+
+}
---
+++ inventory/src/main/java/com/cryptopos/inventory/service/item/ItemServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cryptopos.inventory.service.item;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemServiceImpl implements ItemService {
+
+}

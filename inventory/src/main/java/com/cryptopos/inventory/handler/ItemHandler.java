6db9ae542c2bd98c2e63f61c92ef47c96473ee2d
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-12 18:12:08 +0530

    Add handler method to create an item

--- inventory/src/main/java/com/cryptopos/inventory/handler/ItemHandler.java
+++ inventory/src/main/java/com/cryptopos/inventory/handler/ItemHandler.java
@@ -1,8 +1,37 @@
 package com.cryptopos.inventory.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.cryptopos.inventory.dto.ItemCreateRequest;
+import com.cryptopos.inventory.exceptions.NotPermittedException;
+import com.cryptopos.inventory.service.item.ItemService;
+
+import reactor.core.publisher.Mono;
 
 @Component
 public class ItemHandler {
 
+    private final ItemService itemService;
+
+    public ItemHandler(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    public Mono<ServerResponse> createItem(ServerRequest request) {
+        return request
+                .bodyToMono(ItemCreateRequest.class)
+                .flatMap(createRequest -> itemService.createItem(Long.parseLong(request.pathVariable("branchId")),
+                        createRequest))
+                .flatMap(result -> ServerResponse.status(HttpStatus.CREATED).build())
+                .onErrorResume(error -> {
+                    if (error instanceof NotPermittedException) {
+                        return ServerResponse.status(HttpStatus.FORBIDDEN).build();
+                    }
+
+                    return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+                });
+    }
 }

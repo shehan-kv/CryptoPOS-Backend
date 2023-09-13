2423f0db56d95d1efde91ea9c70c59163d8acfd5
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-13 21:02:56 +0530

    Implement service method to get branches by org in orgs

--- orgs/src/main/java/com/cryptopos/orgs/service/branch/BranchService.java
+++ orgs/src/main/java/com/cryptopos/orgs/service/branch/BranchService.java
@@ -1,11 +1,15 @@
 package com.cryptopos.orgs.service.branch;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.cryptopos.orgs.dto.BranchCreateRequest;
 import com.cryptopos.orgs.dto.BranchCreateResult;
+import com.cryptopos.orgs.dto.BranchResponse;
 import com.cryptopos.orgs.dto.BranchUpdateRequest;
 import com.cryptopos.orgs.dto.BranchUpdateResult;
+import com.cryptopos.orgs.dto.Page;
 
 import reactor.core.publisher.Mono;
 
@@ -15,4 +19,6 @@ public interface BranchService {
     Mono<BranchCreateResult> createBranch(BranchCreateRequest createRequest);
 
     Mono<BranchUpdateResult> updateBranch(Long branchId, BranchUpdateRequest updateRequest);
+
+    Mono<Page<BranchResponse>> getBranchesByOrg(Long orgId, Optional<String> pageNum, Optional<String> pageSize);
 }

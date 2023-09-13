4b8e77d1f54c2ba41428243eb11c235ae2c52dc4
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-13 16:37:11 +0530

    Create NotPermittedException in orgs

---
+++ orgs/src/main/java/com/cryptopos/orgs/exception/NotPermittedException.java
@@ -0,0 +1,8 @@
+package com.cryptopos.orgs.exception;
+
+public class NotPermittedException extends RuntimeException {
+
+    public NotPermittedException() {
+        super("Not Permitted");
+    }
+}

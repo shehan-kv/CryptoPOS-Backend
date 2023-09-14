d75d7348b976ae0f0356d88f7b42a22d477d3b50
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-14 16:39:32 +0530

    Add repo method to get branch currency info

--- orgs/src/main/java/com/cryptopos/orgs/repository/BranchRepository.java
+++ orgs/src/main/java/com/cryptopos/orgs/repository/BranchRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.cryptopos.orgs.dto.BranchResponse;
+import com.cryptopos.orgs.dto.BranchCurrencyResponse;
 import com.cryptopos.orgs.dto.BranchDetailsResponse;
 import com.cryptopos.orgs.entity.Branch;
 
@@ -46,4 +47,13 @@ public interface BranchRepository extends ReactiveCrudRepository<Branch, Long> {
             WHERE b.org_id = :orgId AND eb.employee_id = :userId
             """)
     Mono<Long> countBranchesByOrgIdAndUserId(Long orgId, Long userId);
+
+    @Query("""
+            SELECT b.id, c.code as code
+            FROM currencies c
+            JOIN orgs o ON o.currency_id = c.id
+            JOIN branches b ON b.org_id = o.id
+            WHERE b.id = :branchId
+                """)
+    Mono<BranchCurrencyResponse> findCurrencyById(Long branchId);
 }

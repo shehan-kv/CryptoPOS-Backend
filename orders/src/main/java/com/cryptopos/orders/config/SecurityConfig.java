4dc05a94763b1685bc46075d68e84c08860198ae
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-15 20:38:09 +0530

    Add Security Config in orders

---
+++ orders/src/main/java/com/cryptopos/orders/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.cryptopos.orders.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public MapReactiveUserDetailsService mapReactiveUserDetailsService() {
+        return new MapReactiveUserDetailsService(Collections.emptyMap());
+    }
+
+    @Bean
+    public SecurityWebFilterChain webFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(customizer -> customizer.disable())
+                .httpBasic(customizer -> customizer.disable())
+                .build();
+    }
+}

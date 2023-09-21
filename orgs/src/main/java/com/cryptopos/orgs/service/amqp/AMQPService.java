1b2d40dcc7ca8f3089d720488d4a65900b3dd1e4
Shehan Maduwantha <kvsm066@gmail.com>
2023-09-21 21:13:24 +0530

    Add method to get orgId of branch in orgs amqp service

--- orgs/src/main/java/com/cryptopos/orgs/service/amqp/AMQPService.java
+++ orgs/src/main/java/com/cryptopos/orgs/service/amqp/AMQPService.java
@@ -3,9 +3,9 @@ package com.cryptopos.orgs.service.amqp;
 import java.util.List;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cryptopos.orgs.repository.BranchRepository;
 import com.cryptopos.orgs.repository.EmployeeRepository;
 
 import reactor.core.publisher.Mono;
@@ -13,11 +13,21 @@ import reactor.core.publisher.Mono;
 @Service
 public class AMQPService {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
+    private final BranchRepository branchRepository;
+
+    public AMQPService(EmployeeRepository empRepository, BranchRepository brRepository) {
+        this.employeeRepository = empRepository;
+        this.branchRepository = brRepository;
+    }
 
     @RabbitListener(queues = "user.branches")
     public Mono<List<Long>> getBranches(Long data) {
         return employeeRepository.getBranches(data).collectList();
+    }
+
+    @RabbitListener(queues = "branch.org")
+    public Mono<Long> getBranchOrgId(Long data) {
+        return branchRepository.findOrgIdByBranchId(data);
     }
 }
